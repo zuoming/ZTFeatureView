3e9d3a40065efadbb3ceb473e1c631adb7065080
zuoming <baggio.zuoming@gmail.com>
2016-01-25 13:29:56 +0800

    Initial Commit

---
+++ ZTFeatureMaskExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZTFeatureMaskExample
+//
+//  Created by zuo ming on 16/1/25.
+//  Copyright © 2016年 zuo ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZTFeatureMaskExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZTFeatureMaskExample
+//
+//  Created by zuo ming on 16/1/25.
+//  Copyright © 2016年 zuo ming. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
